17b368a34a448b398cb98c68e526416055343211
Nikolai Perlt <nikolai.perlt@visma.com>
2022-05-04 15:57:42 +0200

    refactor and auto breaking

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -1,19 +1,27 @@
-/* sudo arduino-cli compile --upload -b arduino:avr:uno -p /dev/ttyUSB0 */
+/* sudo arduino-cli compile --upload -b arduino:avr:uno -p /dev/ttyUSB0  &&  sudo cat /dev/ttyUSB0 */
 
 #include <Servo.h>
 #include <SoftwareSerial.h>
+#include <Ultrasonic.h>
 #include <arduino-timer.h>
 
-SoftwareSerial BTSensor(11, 10); // TX | RX
+SoftwareSerial BTSensor(11, 10);  // TX | RX
 Servo Servo1;
+auto timer = timer_create_default();
+Ultrasonic ultrasonic(3, 2);
 
 char command = ' ';
 const int MOTOR_SWITCH = 8;
 const int RATE = 9600;
-const int SERVO_PIN = 3;
-const int RIGHT_BLINK = 4;
+const int SERVO_PIN = 13;
 
-auto timer = timer_create_default();
+const int TAIL_LIGHTS = 6;
+
+const int LEFT_BLINK = 5;
+const int RIGHT_BLINK = 7;
+
+unsigned int LEFT_BLINK_TIMER = 0;
+unsigned int RIGHT_BLINK_TIMER = 0;
 
 void setup() {
   Serial.begin(RATE);
@@ -23,6 +31,8 @@ void setup() {
 
   pinMode(MOTOR_SWITCH, OUTPUT);
   pinMode(RIGHT_BLINK, OUTPUT);
+  pinMode(LEFT_BLINK, OUTPUT);
+  pinMode(TAIL_LIGHTS, OUTPUT);
 
   Serial.println("Ready to connect\nDefualt password is 1234 or 000");
 }
@@ -33,35 +43,65 @@ bool blink(void *argument) {
   return true;
 }
 
-void loop() {
-  timer.tick();
-  delay(100);
+void resetTurnLights() {
+  timer.cancel(RIGHT_BLINK_TIMER);
+  timer.cancel(LEFT_BLINK_TIMER);
+  digitalWrite(RIGHT_BLINK, LOW);
+  digitalWrite(LEFT_BLINK, LOW);
+}
+
+void handleBluetooth() {
   if (BTSensor.available()) {
     command = BTSensor.read();
     Serial.println(command);
   }
+}
 
-  switch (command) {
-  case 'f':
-    Serial.println("Forward!");
-    digitalWrite(MOTOR_SWITCH, HIGH);
-    Servo1.write(100);
-    digitalWrite(RIGHT_BLINK, LOW);
-    break;
-  case 'd':
-    Serial.println("Reversing!");
-    digitalWrite(MOTOR_SWITCH, LOW);
-    break;
-  case 'l':
-    Serial.println("Left!");
-    Servo1.write(145);
-    break;
-  case 'r':
-    Serial.println("Right!");
-    Servo1.write(55);
-    timer.every(1000, blink, (void *)RIGHT_BLINK);
-    break;
+void handleTailLights() {
+  if (!digitalRead(MOTOR_SWITCH)) {
+    digitalWrite(TAIL_LIGHTS, HIGH);
+  } else {
+    digitalWrite(TAIL_LIGHTS, LOW);
   }
+}
 
+void handleAutoBreak() {
+  auto distance = ultrasonic.read();
+  Serial.println(("distance " + String(distance)));
+  if (distance <= 5) {
+    digitalWrite(MOTOR_SWITCH, LOW);
+  }
+}
+
+void handleCommands() {
+  switch (command) {
+    case 'f':
+      digitalWrite(MOTOR_SWITCH, HIGH);
+      Servo1.write(100);
+      resetTurnLights();
+      break;
+    case 'd':
+      digitalWrite(MOTOR_SWITCH, LOW);
+      break;
+    case 'l':
+      Servo1.write(145);
+      resetTurnLights();
+      LEFT_BLINK_TIMER = timer.every(500, blink, (void *)LEFT_BLINK);
+      break;
+    case 'r':
+      Servo1.write(65);
+      resetTurnLights();
+      RIGHT_BLINK_TIMER = timer.every(500, blink, (void *)RIGHT_BLINK);
+      break;
+  }
   command = ' ';
+}
+
+void loop() {
+  delay(50);
+  timer.tick();
+  handleBluetooth();
+  handleTailLights();
+  handleCommands();
+  /* handleAutoBreak(); */
 }
